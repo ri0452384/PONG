3268f50650603e884854360efce8a6d2ede7a53a
ri0452384 <rayven.ingles@gmail.com>
2016-10-26 04:39:06 +0800

    Update Racket2.java

--- src/src/pong/Racket2.java
+++ src/src/pong/Racket2.java
@@ -1,9 +1,9 @@
 package pong;
 /*
- * This class is used for the racket2 object which is controlled by the user and it only moves left to right
- * also has a separate listener method to check for keyboard input by the user
+ * This class is used for the racket2 object which is controlled by the user and it only moves up and down.
+ * It also has a separate listener method to check for keyboard input by the user
  * 
- * Racquet2 will be vertical instead of horizontal and the up and down arrow keys on the keyboard allow it to move up and down
+ * Racquet2 can be controlled either by up or down arrow keys, or W and S keys on the keyboard.
  * Added ultimate: players can now press the Q or Page Down button to use ultimate, which would make the ball go faster
  * once the ball touches the racket.
  * 
@@ -16,15 +16,15 @@ import java.awt.Rectangle;
 import java.awt.event.KeyEvent;
 
 public class Racket2{
-	
+	//finalized dimensions of each racket2  object
 	private static final int WIDTH = 10;
 	private static final int HEIGHT = 100;
-	
+	//removed static and final from x and y to allow creating of multiple racket2 objects in a single table.
 	int x;
 	int y;
 	int ya;
 	private GameOfPong game;
-	
+	//variables for the ultimate
 	protected boolean ultUsed;
 	private int ultCount;
 	
@@ -53,16 +53,18 @@ public class Racket2{
 
 	public void keyPressed(KeyEvent e) {
 	
-		
+		//event handling is the same for each direction
 		if (e.getKeyCode() == KeyEvent.VK_UP || e.getKeyCode() == KeyEvent.VK_W){
-			
+			//added a 3 to the vector to make racket movement faster
 			ya = -game.speed -3;
 			
 		}
 		if (e.getKeyCode() == KeyEvent.VK_DOWN || e.getKeyCode() == KeyEvent.VK_S){
+			//added a 3 to the vector to make racket movement faster
 			ya = game.speed + 3;
 			
 		}
+		//event handling for the ultimate button if pressed
 		if(e.getKeyCode() == KeyEvent.VK_PAGE_DOWN || e.getKeyCode() == KeyEvent.VK_Q){
 			if(ultUsed == false && ultCount > 0){
 				ultUsed = true;
